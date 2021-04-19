09d4c9da99c9492bec8050e95a568a7f3915cbeb
kurokeita <kuro.keita94@gmail.com>
2021-04-19 23:10:58 +0700

    change card style

--- pages/index.js
+++ pages/index.js
@@ -25,32 +25,87 @@ export default function Home() {
                             <Avatar size={{xs: 200, sm: 200, md: 400, lg: 400, xl: 400, xxl: 400}}
                                     src={<Image src='/avatar.jpg'/>}/>
                         </Row>
-                        <Row justify='center' gutter={[16,16]}>
-                            <Col md={6} lg={3} xs={18} type='flex' align='center'>
+                        <Row justify='center'>
+                            <div className='grid'>
                                 <Link href='/me'>
-                                    <Card style={{textAlign: 'center'}} hoverable bordered>
+                                    <a className='card'>
                                         <h1>About me &rarr;</h1>
-                                    </Card>
+                                    </a>
                                 </Link>
-                            </Col>
-                            <Col md={6} lg={3} xs={18} type='flex' align='center'>
                                 <Link href='/works'>
-                                    <Card style={{textAlign: 'center'}} hoverable bordered>
+                                    <a className='card'>
                                         <h1>My works &rarr;</h1>
-                                    </Card>
+                                    </a>
                                 </Link>
-                            </Col>
-                            <Col md={6} lg={3} xs={18} type='flex' align='center'>
                                 <Link href='/contact'>
-                                    <Card style={{textAlign: 'center'}} hoverable bordered>
+                                    <a className='card'>
                                         <h1>Contact me &rarr;</h1>
-                                    </Card>
+                                    </a>
                                 </Link>
-                            </Col>
+                            </div>
                         </Row>
                     </Content>
                 </Layout>
             </main>
+
+            <style jsx>{`
+
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
+
+              .grid {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: wrap;
+
+                max-width: 800px;
+                margin-top: 3rem;
+              }
+
+              .card {
+                margin: 1rem;
+                flex-basis: 45%;
+                padding: 1.5rem;
+                text-align: center;
+                color: inherit;
+                text-decoration: none;
+                border: 1px solid #c8a6ec;
+                border-radius: 10px;
+                transition: color 0.15s ease, border-color 0.15s ease;
+              }
+
+              .card:hover,
+              .card:focus,
+              .card:active {
+                color: #0070f3;
+                border-color: #0070f3;
+              }
+
+              .card h3 {
+                margin: 0 0 1rem 0;
+                font-size: 1.5rem;
+              }
+
+              .card p {
+                margin: 0;
+                font-size: 1.25rem;
+                line-height: 1.5;
+              }
+
+              .logo {
+                height: 1em;
+              }
+
+              @media (max-width: 600px) {
+                .grid {
+                  width: 100%;
+                  flex-direction: column;
+                }
+              }
+            `}</style>
         </div>
     )
 }
